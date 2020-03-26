fb8fd683cd709e0bc8e944001a0b21b4c3ec1306
Maheshwari02 <62683942+Maheshwari02@users.noreply.github.com>
2020-03-26 14:11:05 +0530

    Update Sample.java

--- GitProject/src/com/git/Sample.java
+++ GitProject/src/com/git/Sample.java
@@ -3,5 +3,7 @@ package com.git;
 public class Sample {
 public static void main(String[] args) {
 	System.out.println("This is a Sample git operation");
+	
+	System.out.println("This is the changes made in GitHub")
 }
 }
